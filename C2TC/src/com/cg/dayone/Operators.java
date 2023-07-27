daa5604ae36e97cb1e8c10cc2433d8484467d7de
Karan <Karan@karan>
2023-07-27 10:13:24 +0530

    First commit

---
+++ C2TC/src/com/cg/dayone/Operators.java
@@ -0,0 +1,31 @@
+// use of arithmetic(%), assignment(=),relational(==,<) operators
+package com.cg.dayone;
+import java.util.*;
+public class Operators {
+	  public static void main(String[]args)
+	  {
+		
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("enter the value:");
+		 int n = sc.nextInt();
+		 int flag=0;
+		 for(int i=2;i<n;i++)
+		 {
+			 if(n%i==0)
+			 {
+				 flag=1;
+			 }
+		 }
+			 if(flag==0)
+			 {
+				 System.out.println("It is a prime number");
+			 }
+			 else 
+			 {
+				 System.out.println("Not a prime");
+			 }
+		 
+		  
+	  }
+
+}
---
+++ C2TC/src/daytwo/New.java
@@ -0,0 +1,15 @@
+package daytwo;
+import java.util.*;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner s =new Scanner(System.in);
+		int n = s.nextInt();
+		
+
+	}
+
+}
